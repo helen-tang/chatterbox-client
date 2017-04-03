7f40d8907dc1554c5cf948922afee49eb8fd0416
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-03 14:25:13 -0700

    added skeletion methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,104 @@
 // YOUR CODE HERE:
 
+var App = function() {
+  
+};
+
+App.prototype.init = function() {
+  
+};
+
+App.prototype.send = function() {
+  
+};
+
+App.prototype.fetch = function() {
+  
+};
+
+App.prototype.clearMessages = function() {
+  
+};
+
+App.prototype.renderMessage = function() {
+  
+};
+
+App.prototype.renderRoom = function() {
+  
+};
+
+App.prototype.handleUsernameClick = function() {
+  
+};
+
+App.prototype.handleSubmit = function() {
+  
+};
+var app = new App();
 
 
+$(document).ready(function() {
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message recieved');
+      console.log(data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+  
+  $('#refresh').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message recieved');
+        console.log(data);
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  });
+
+
+  $('#postMessage').on('click', function(event) {
+    event.preventDefault();
+    
+    var message = {
+      username: 'ak47hax1337',
+      text: $('textarea#exampleMessage').val(),
+      roomname: 'lobby'
+    };
+
+    $.ajax({
+      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  });
 });
+
 
