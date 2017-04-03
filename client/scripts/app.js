c7c0e739f014dcdd2c9a86057bd211664bb33522
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-03 13:36:02 -0700

    added stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,20 @@
 // YOUR CODE HERE:
+
+
+
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
