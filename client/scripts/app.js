09a5551ff2910d9e1c27b9daefa9c4dd6e23eccb
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-04-03 15:19:11 -0700

    added stuff

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,45 +1,47 @@
 // YOUR CODE HERE:
 
 var App = function() {
-  
+  this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
 };
 
 App.prototype.init = function() {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message recieved');
+      console.log(data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
   
 };
 
-App.prototype.send = function() {
+App.prototype.send = function(message) {
+
+  $.ajax({
+    url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
   
 };
 
 App.prototype.fetch = function() {
-  
-};
-
-App.prototype.clearMessages = function() {
-  
-};
-
-App.prototype.renderMessage = function() {
-  
-};
-
-App.prototype.renderRoom = function() {
-  
-};
-
-App.prototype.handleUsernameClick = function() {
-  
-};
-
-App.prototype.handleSubmit = function() {
-  
-};
-var app = new App();
-
-
-$(document).ready(function() {
-
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
@@ -56,49 +58,71 @@ $(document).ready(function() {
     }
   });
 
+};
+
+App.prototype.clearMessages = function() {
+
+  $('#chats').remove();
+  
+};
+
+App.prototype.renderMessage = function(message) {
+  
+  var username = message.username;
+  var created = message.createdAt;
+  var roomname = message.roomname;
+  var text = message.text;
+
+  var elementToAppend = `<div class="row">
+                            <div class="four columns">
+                              <h3>` + username + `</h3>
+                            </div>
+                            <div class="four columns">
+                              <h3>` + created + `</h3>
+                            </div>
+                            <div class="six columns">
+                              <p>` + text + `</p>
+                            </div>
+                          </div>`;
+
+  console.log(elementToAppend);
+  console.log($('#chats'));
+
+  // $('#chats').append(elementToAppend);
+  $('div #chats').parent().append(elementToAppend);
+
+};
+
+App.prototype.renderRoom = function() {
+  
+};
+
+App.prototype.handleUsernameClick = function() {
+  
+};
+
+App.prototype.handleSubmit = function() {
+  
+};
+
+var app = new App();
+
+$(document).ready(function() {
+
+  app.init();
+
   
   $('#refresh').on('click', function(event) {
     event.preventDefault();
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message recieved');
-        console.log(data);
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
+    app.fetch();
   });
 
 
   $('#postMessage').on('click', function(event) {
     event.preventDefault();
-    
-    var message = {
-      username: 'ak47hax1337',
-      text: $('textarea#exampleMessage').val(),
-      roomname: 'lobby'
-    };
-
-    $.ajax({
-      url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-      type: 'POST',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message sent');
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to send message', data);
-      }
-    });
+    app.send();
   });
+
 });
 
 
